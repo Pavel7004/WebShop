1ca4379e20583d0cf2193e86786c3957b480af21
Kovalev Pavel <kovalev5690@gmail.com>
2022-03-06 18:45:13 +0300

    Add tracing to shop component

--- pkg/components/shop/shop.go
+++ pkg/components/shop/shop.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/Pavel7004/Common/tracing"
 	dbi "github.com/Pavel7004/WebShop/pkg/adapters/db"
 	"github.com/Pavel7004/WebShop/pkg/components"
 	"github.com/Pavel7004/WebShop/pkg/domain"
@@ -22,17 +23,29 @@ func New(db dbi.DB) *Shop {
 }
 
 func (s *Shop) GetItemById(ctx context.Context, id string) (*domain.Item, error) {
+	span, ctx := tracing.StartSpanFromContext(ctx)
+	defer span.Finish()
+
 	return s.db.GetItemById(ctx, id)
 }
 
 func (s *Shop) AddItem(ctx context.Context, item *domain.AddItemRequest) (string, error) {
+	span, ctx := tracing.StartSpanFromContext(ctx)
+	defer span.Finish()
+
 	return s.db.AddItem(ctx, item)
 }
 
 func (s *Shop) GetItemsByPrice(ctx context.Context, from, to float64) ([]*domain.Item, error) {
+	span, ctx := tracing.StartSpanFromContext(ctx)
+	defer span.Finish()
+
 	return s.db.GetItemsByPrice(ctx, from, to)
 }
 
 func (s *Shop) GetRecentlyAddedItems(ctx context.Context, period time.Duration) ([]*domain.Item, error) {
+	span, ctx := tracing.StartSpanFromContext(ctx)
+	defer span.Finish()
+
 	return s.db.GetRecentlyAddedItems(ctx, period)
 }
