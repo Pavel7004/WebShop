841181a56b10a06ba6fbe5f67b6e8deece73fc46
Kovalev Pavel <kovalev5690@gmail.com>
2022-03-25 12:53:28 +0300

    Add user mongo model

---
+++ pkg/adapters/db/mongo/models/user.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"time"
+
+	"github.com/Pavel7004/WebShop/pkg/domain"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	Name      string             `bson:"name"`
+	Email     string             `bson:"email"`
+	Phone     string             `bson:"phone"`
+	CreatedAt time.Time          `bson:"created_at"`
+}
+
+func (user *User) ConvertToDomain() *domain.User {
+	return &domain.User{
+		ID:        user.ID.Hex(),
+		Name:      user.Name,
+		Email:     user.Email,
+		Phone:     user.Phone,
+		CreatedAt: user.CreatedAt,
+	}
+}
+
+func ConvertUsersToDomain(users []User) []*domain.User {
+	result := make([]*domain.User, 0, len(users))
+
+	for _, user := range users {
+		result = append(result, user.ConvertToDomain())
+	}
+
+	return result
+}
