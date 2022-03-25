beaa143cd6827057527ed5cf886c473649dcdfbe
Kovalev Pavel <kovalev5690@gmail.com>
2022-03-25 13:03:38 +0300

    Move users related code to users.go

---
+++ pkg/adapters/db/mongo/users.go
@@ -0,0 +1,70 @@
+package mongo
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/Pavel7004/Common/tracing"
+	"github.com/Pavel7004/WebShop/pkg/adapters/db/mongo/models"
+	"github.com/Pavel7004/WebShop/pkg/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func (db *DB) RegisterUser(ctx context.Context, user *domain.RegisterUserRequest) (string, error) {
+	span, ctx := tracing.StartSpanFromContext(ctx)
+	defer span.Finish()
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	res, err := db.collectionUsers.InsertOne(ctx, bson.M{
+		"name":       user.Name,
+		"email":      user.Email,
+		"phone":      user.Phone,
+		"created_at": time.Now(),
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	obj, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return "", domain.ErrInvalidId
+	}
+
+	span.SetTag("result_id", obj.Hex())
+
+	return obj.Hex(), nil
+}
+
+func (db *DB) GetUserByID(ctx context.Context, id string) (*domain.User, error) {
+	span, ctx := tracing.StartSpanFromContext(ctx)
+	defer span.Finish()
+
+	span.SetTag("user_id", id)
+
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, domain.ErrInvalidId
+	}
+
+	span.SetTag("object_id", objectID)
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	var result models.User
+	if err := db.collectionUsers.FindOne(ctx, bson.M{"_id": objectID}).Decode(&result); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, domain.ErrUserNotFound
+		}
+
+		return nil, err
+	}
+
+	return result.ConvertToDomain(), nil
+}
