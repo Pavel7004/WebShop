d689dc94c803eacc68d02623461feba9e8605c4e
Kovalev Pavel <kovalev5690@gmail.com>
2022-03-25 13:29:39 +0300

    Add user functions to DB interface

--- pkg/adapters/db/db.go
+++ pkg/adapters/db/db.go
@@ -8,9 +8,13 @@ import (
 )
 
 type DB interface {
-	GetItemById(ctx context.Context, id string) (*domain.Item, error)
 	AddItem(ctx context.Context, item *domain.AddItemRequest) (string, error)
+	GetItemById(ctx context.Context, id string) (*domain.Item, error)
 	GetItemsByPrice(ctx context.Context, from, to float64) ([]*domain.Item, error)
 	GetRecentlyAddedItems(ctx context.Context, period time.Duration) ([]*domain.Item, error)
+
+	RegisterUser(ctx context.Context, user *domain.RegisterUserRequest) (string, error)
+	GetUserById(ctx context.Context, id string) (*domain.User, error)
+
 	Close() error
 }
