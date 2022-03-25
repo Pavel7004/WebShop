4c5d67876e259df0ccf79f92e302bcd21b58c87f
Kovalev Pavel <kovalev5690@gmail.com>
2022-03-25 12:28:54 +0300

    Add owner id to domain item

--- pkg/domain/item.go
+++ pkg/domain/item.go
@@ -4,6 +4,7 @@ import "time"
 
 type Item struct {
 	ID          string    `json:"id"`
+	OwnerID     string    `json:"owner_id"`
 	Name        string    `json:"name"`
 	Description string    `json:"desc"`
 	Price       float64   `json:"price"`
@@ -12,6 +13,7 @@ type Item struct {
 
 type AddItemRequest struct {
 	Name        string  `json:"name"`
+	OwnerID     string  `json:"owner_id"`
 	Description string  `json:"desc"`
 	Price       float64 `json:"price"`
 }
