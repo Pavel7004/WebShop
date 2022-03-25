254e414fa2186294064156299a0b0e8876cf6d5b
Kovalev Pavel <kovalev5690@gmail.com>
2022-03-25 13:09:44 +0300

    Pass config to mongo instance

--- pkg/adapters/db/mongo/db.go
+++ pkg/adapters/db/mongo/db.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Pavel7004/WebShop/pkg/adapters/db"
 	"github.com/Pavel7004/WebShop/pkg/adapters/db/mongo/models"
 	"github.com/Pavel7004/WebShop/pkg/domain"
+	"github.com/Pavel7004/WebShop/pkg/infra/config"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -19,23 +20,25 @@ var _ db.DB = (*DB)(nil)
 
 type DB struct {
 	client *mongo.Client
+	cfg    config.Config
 
 	collectionItems *mongo.Collection
 	collectionUsers *mongo.Collection
 }
 
-func New() *DB {
+func New(cfg config.Config) *DB {
 	db := new(DB)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.Mongo.Uri))
 	if err != nil {
 		panic(err)
 	}
 
 	db.client = client
+	db.cfg = cfg
 	db.collectionItems = client.Database("shop").Collection("items")
 	db.collectionUsers = client.Database("shop").Collection("users")
 
