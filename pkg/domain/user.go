10272855ff7bc9eb880c5045d456796579d34796
Kovalev Pavel <kovalev5690@gmail.com>
2022-03-25 12:28:39 +0300

    Fix typo

--- pkg/domain/user.go
+++ pkg/domain/user.go
@@ -3,11 +3,11 @@ package domain
 import "time"
 
 type User struct {
-	ID         string    `json:"id"`
-	Name       string    `json:"name"`
-	Email      string    `json:"email"`
-	Phone      string    `json:"phone"`
-	Created_at time.Time `json:"created_at"`
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Phone     string    `json:"phone"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type RegisterUserRequest struct {
