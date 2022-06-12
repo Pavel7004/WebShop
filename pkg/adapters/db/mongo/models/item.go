4e91d8fa567c7f8f9931ceae48f939ed56cc0fa9
Kovalev Pavel <kovalev5690@gmail.com>
2022-06-12 22:14:53 +0300

    Add domain to db model converter

--- pkg/adapters/db/mongo/models/item.go
+++ pkg/adapters/db/mongo/models/item.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/Pavel7004/WebShop/pkg/domain"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -40,6 +41,29 @@ func ConvertItemFromDomainRequest(it *domain.AddItemRequest) (*Item, error) {
 	}, nil
 }
 
+func ConvertItemFromDomain(it *domain.Item) (*Item, error) {
+	id, err := primitive.ObjectIDFromHex(it.ID)
+	if err != nil {
+		return nil, domain.ErrInvalidId
+	}
+
+	ownerId, err := primitive.ObjectIDFromHex(it.OwnerID)
+	if err != nil {
+		return nil, domain.ErrInvalidId
+	}
+
+	return &Item{
+		ID:          id,
+		OwnerID:     ownerId,
+		Name:        it.Name,
+		Description: it.Description,
+		Category:    it.Category,
+		Price:       it.Price,
+		CreatedAt:   it.CreatedAt,
+		Quantity:    it.Quantity,
+	}, nil
+}
+
 func (it *Item) ConvertToDomain() *domain.Item {
 	return &domain.Item{
 		ID:          it.ID.Hex(),
@@ -61,4 +85,31 @@ func ConvertItemsToDomain(items []Item) []*domain.Item {
 	}
 
 	return result
+}
+
+func ConvertUpdateReqToBSON(in *domain.UpdateItemRequest) (bson.M, error) {
+	if in == nil {
+		return nil, domain.ErrNoUpdate
+	}
+	req := bson.M{}
+	if in.Name != nil {
+		req["name"] = in.Name
+	}
+	if in.Description != nil {
+		req["desc"] = in.Description
+	}
+	if in.Category != nil {
+		req["category"] = in.Category
+	}
+	if in.OwnerID != nil {
+		req["owner_id"] = in.OwnerID
+	}
+	if in.Price != nil {
+		req["price"] = in.Price
+	}
+	if in.Quantity != nil {
+		req["quantity"] = in.Quantity
+	}
+	req = bson.M{"$set": req}
+	return req, nil
 }
