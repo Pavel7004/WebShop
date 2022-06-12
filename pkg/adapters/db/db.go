1eb3539ded663e954e83059eb8144170a3294086
Kovalev Pavel <kovalev5690@gmail.com>
2022-06-12 22:33:12 +0300

    Add GetItemsTotalCost to db interface

--- pkg/adapters/db/db.go
+++ pkg/adapters/db/db.go
@@ -14,6 +14,7 @@ type DB interface {
 	GetItemsByPrice(ctx context.Context, from, to float64) ([]*domain.Item, error)
 	GetRecentlyAddedItems(ctx context.Context, period time.Duration) ([]*domain.Item, error)
 	GetItemsByOwnerId(ctx context.Context, id string) ([]*domain.Item, error)
+	GetItemsTotalCost(ctx context.Context, itemIDs []string) (float64, error)
 
 	RegisterUser(ctx context.Context, user *domain.RegisterUserRequest) (string, error)
 	GetUserById(ctx context.Context, id string) (*domain.User, error)
