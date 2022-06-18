c2364be33325caaf6425c7eb963dc28e018c12b0
Kovalev Pavel <kovalev5690@gmail.com>
2022-06-18 20:55:14 +0300

    Add order processing related errors

--- pkg/domain/errors.go
+++ pkg/domain/errors.go
@@ -1,13 +1,15 @@
 package domain
 
 var (
-	ErrItemNotFound      = NewError(404, "item_not_found", "Item not found")
-	ErrInvalidId         = NewError(404, "invalid_id", "Can't parse id string")
-	ErrUserNotFound      = NewError(404, "user_not_found", "User not found")
-	ErrNoUpdate          = NewError(404, "update_not_specified", "There are no updates")
-	ErrOrderNotProcessed = NewError(404, "order_not_processed", "Order not processed")
-	ErrOrderNotFound     = NewError(404, "order_not_found", "Order not found")
-	ErrNoOrder           = NewError(404, "order_not_provided", "Order is nil")
+	ErrItemNotFound          = NewError(404, "item_not_found", "Item not found")
+	ErrInvalidId             = NewError(404, "invalid_id", "Can't parse id string")
+	ErrUserNotFound          = NewError(404, "user_not_found", "User not found")
+	ErrNoUpdate              = NewError(404, "update_not_specified", "There are no updates")
+	ErrOrderNotProcessed     = NewError(404, "order_not_processed", "Order not processed")
+	ErrOrderNotFound         = NewError(404, "order_not_found", "Order not found")
+	ErrNoOrder               = NewError(404, "order_not_provided", "Order is nil")
+	ErrOrderNotPaid          = NewError(404, "order_not_paid", "Order isn't paid")
+	ErrOrderAlreadyDelivered = NewError(404, "order_delivered", "Order already delivered")
 )
 
 type Error struct {
