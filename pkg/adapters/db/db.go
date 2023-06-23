2b45fa9f36c860731bff7e9d04622617deece7ca
Kovalev Pavel <kovalev5690@gmail.com>
2023-06-23 19:26:24 +0300

    Split DB interface into separate interfaces

--- pkg/adapters/db/db.go
+++ pkg/adapters/db/db.go
@@ -7,22 +7,34 @@ import (
 	"github.com/Pavel7004/WebShop/pkg/domain"
 )
 
-type DB interface {
-	AddItem(ctx context.Context, item *domain.AddItemRequest) (string, error)
-	UpdateItem(ctx context.Context, id string, in *domain.UpdateItemRequest) (int64, error)
-	GetItemById(ctx context.Context, id string) (*domain.Item, error)
-	GetItemsByPrice(ctx context.Context, from, to float64) ([]*domain.Item, error)
-	GetRecentlyAddedItems(ctx context.Context, period time.Duration) ([]*domain.Item, error)
-	GetItemsByOwnerId(ctx context.Context, id string) ([]*domain.Item, error)
-	GetItemsTotalCost(ctx context.Context, itemIDs []string) (float64, error)
+type (
+	DB interface {
+		Item
+		User
+		Order
 
-	RegisterUser(ctx context.Context, user *domain.RegisterUserRequest) (string, error)
-	GetUserById(ctx context.Context, id string) (*domain.User, error)
-	GetRecentlyAddedUsers(ctx context.Context, count int64) ([]*domain.User, error)
+		Close() error
+	}
 
-	CreateOrder(ctx context.Context, req *domain.CreateOrderRequest) (string, error)
-	GetOrderInfo(ctx context.Context, id string) (*domain.Order, error)
-	UpdateOrder(ctx context.Context, id string, ord domain.UpdateOrderRequest) (int64, error)
+	Item interface {
+		AddItem(ctx context.Context, item *domain.AddItemRequest) (string, error)
+		UpdateItem(ctx context.Context, id string, in *domain.UpdateItemRequest) (int64, error)
+		GetItemById(ctx context.Context, id string) (*domain.Item, error)
+		GetItemsByPrice(ctx context.Context, from, to float64) ([]*domain.Item, error)
+		GetRecentlyAddedItems(ctx context.Context, period time.Duration) ([]*domain.Item, error)
+		GetItemsByOwnerId(ctx context.Context, id string) ([]*domain.Item, error)
+		GetItemsTotalCost(ctx context.Context, itemIDs []string) (float64, error)
+	}
 
-	Close() error
-}
+	User interface {
+		RegisterUser(ctx context.Context, user *domain.RegisterUserRequest) (string, error)
+		GetUserById(ctx context.Context, id string) (*domain.User, error)
+		GetRecentlyAddedUsers(ctx context.Context, count int64) ([]*domain.User, error)
+	}
+
+	Order interface {
+		CreateOrder(ctx context.Context, req *domain.CreateOrderRequest) (string, error)
+		GetOrderInfo(ctx context.Context, id string) (*domain.Order, error)
+		UpdateOrder(ctx context.Context, id string, ord domain.UpdateOrderRequest) error
+	}
+)
